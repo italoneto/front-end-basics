c5660146957f427dbc9e99d2958796542b291821
ItaloNetoSantos <56045456+ItaloNetoSantos@users.noreply.github.com>
2019-12-13 08:32:48 -0300

    Adicionando js

---
+++ js/logic.js
@@ -0,0 +1,24 @@
+function gerar() {
+    var valor = [];
+    var num = Math.floor(Math.random((valor)) * 100);
+    return num;
+}
+
+function random() {
+    for (i = 0; i < 100; i++)
+        document.write(gerar() + "</br>")
+}
+
+
+var ordCrescente = [20, 30, 70, 40, 100, 90];
+
+ ordenarCrescent.sort(function(a, b){
+    return a - b
+ })
+
+var ordDecrescente = [10,20,30,40,50,60]
+
+ordDecrescente.sort(function(a, b){
+    return a - b
+ })
+
